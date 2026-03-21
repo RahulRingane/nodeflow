13fa5c2853a66125dc1c9383d52b4a62baac85a1
Rahul Ringane <rahul897r@gmail.com>
2026-03-21 12:54:16 +0530

    Workflow-CRUD

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,7 +1,25 @@
+import { WorkflowList, WorkflowsContainer } from "@/features/workflows/components/workflow";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
+import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-const Page = () => {
+const Page = async () => {
+    await requireAuth();
+
+    prefetchWorkflows();
+
     return (
-        <p> WorkFlow </p>
+        <HydrateClient>
+            <ErrorBoundary fallback={<p>error</p>}>
+                <Suspense fallback={<p>loading...</p>}>
+                  <WorkflowsContainer>
+                     <WorkflowList/>
+                  </WorkflowsContainer>
+                </Suspense>
+            </ErrorBoundary>
+        </HydrateClient>
     )
 };
 
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,47 +1,10 @@
-import { z } from 'zod';
-import { createTRPCRouter, protectedProcedure } from '../init';
-import prisma from '@/lib/db';
-import { inngest } from '@/inngest/client';
-import { google } from "@ai-sdk/google"
-import { LanguageModelV3 } from '@ai-sdk/provider';
-import { generateText as aiGenerateText } from 'ai';
+import { workFlowsRouter } from "@/features/workflows/server/routers";
+import { createTRPCRouter } from "../init";
+
+
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
-   await inngest.send({
-    name: "execute/ai",
-   })
-   
-   return { success: true, message: "Job Queued"}
-  }),
+  workflows: workFlowsRouter,
+})
 
-  getWorkflows: protectedProcedure
-    .query(({ ctx }) => {
-      return prisma.workFlow.findMany();
-    }),
-
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "r@gmail.com"
-      }
-    })
-    return prisma.workFlow.create({
-      data: {
-        name: "test-workflow"
-      },
-    })
-  }),
-});
-
-// export type definition of API
 export type AppRouter = typeof appRouter;
-
-async function generateText(arg0: { model: LanguageModelV3; prompt: string; }): Promise<{ text: string }> {
-  const result = await aiGenerateText({
-    model: arg0.model,
-    prompt: arg0.prompt,
-  });
-  return { text: result.text };
-}
